657a1f4e1e1374e3a9ffed9dd02bda889ef7556c
MadHawkx <40322760+MadHawkx@users.noreply.github.com>
2021-01-07 21:57:41 +0530

    Update settings.py

--- back/back/settings.py
+++ back/back/settings.py
@@ -136,18 +136,12 @@ WSGI_APPLICATION = 'back.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '', # Enter database name here
-        'USER': '', # Enter your username
-        'PASSWORD': '', # Enter database password
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
-
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
